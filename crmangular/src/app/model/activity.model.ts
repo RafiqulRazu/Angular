fcd546a8336c19c878e6efd90dc9ffac7bc68639
RafiqulRazu <158472447+RafiqulRazu@users.noreply.github.com>
2024-11-21 05:20:33 +0600

    add

--- crmangular/src/app/model/activity.model.ts
+++ crmangular/src/app/model/activity.model.ts
@@ -3,9 +3,9 @@ import { UserModel } from "./user.model";
 
 export class ActivityModel {
     id!: number;
-    activityType!: string; // 'Call', 'Email', 'Meeting'
+    activityType!: string; 
     description!: string;
-    activityDate!: string; // Use string or Date depending on how you handle dates
-    customer!: CustomerModel; // Reference to Customer object
-    agent!: UserModel; // Reference to User object (agent)
+    activityDate!: string; 
+    customer!: CustomerModel;
+    agent!: UserModel; 
   }
--- crmangular/src/app/model/order.model.ts
+++ crmangular/src/app/model/order.model.ts
@@ -1,3 +1,32 @@
+// import { CustomerModel } from './customer.model';
+// import { UserModel } from './user.model';
+// import { orderItemModel } from './orderItem.model';
+
+// export class OrderModel {
+//   id!: number;
+//   leadId?: number; // Assuming Lead is optional in Angular
+//   customer!: CustomerModel;
+//   soldBy!: UserModel;
+//   orderItems!: orderItemModel[];
+//   orderDate!: Date;
+//   totalAmount!: number;
+//   status!: OrderStatus;
+
+//   constructor() {
+//     this.orderItems = [];
+//   }
+// }
+
+// export enum OrderStatus {
+//   PENDING = 'PENDING',
+//   APPROVED = 'APPROVED',
+//   REJECTED = 'REJECTED',
+//   DELIVERED = 'DELIVERED',
+// }
+
+
+
+
 import { LeadModel } from "./lead.model";
 import { ProductModel } from "./product.model";
 
